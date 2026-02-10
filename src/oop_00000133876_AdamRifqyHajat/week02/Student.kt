00b8b92f17ce41c93226270eefa30efeb6474911
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-02-10 15:18:46 +0700

    week 02: add warning validation in init block

--- src/oop_00000133876_AdamRifqyHajat/week02/Student.kt
+++ src/oop_00000133876_AdamRifqyHajat/week02/Student.kt
@@ -1,2 +1,13 @@
 package oop_00000133876_AdamRifqyHajat.week02
 
+class Student (val name: String, val nim: String, var major: String) {
+    init {
+        // Validasi Sederhana: Cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING Object tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Object student $name berhasil dialokasikan di Memory.")
+        }
+    }
+}
