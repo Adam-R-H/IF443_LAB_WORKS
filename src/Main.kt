8c4bc8146ebb2d355424ffb0666a1a67afd1daf6
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-02-11 11:28:45 +0700

    week 02: (task 1) create Loan class with default args

--- src/Main.kt
+++ src/Main.kt
@@ -42,8 +42,8 @@ fun main(){
     } else if (type == 2) {
         // Memanggil Secondary Constructor, jurusan otomatis "Non-Matriculated"
         val s2 = Student(name, nim)
-            println("Terdaftar di ${s2.major} dengan GPA awal ${s2.gpa}")
-        } else {
-            println("Pilihan ngawur, pendaftaran batal!")
+        println("Terdaftar di ${s2.major} dengan GPA awal ${s2.gpa}")
+    } else {
+        println("Pilihan ngawur, pendaftaran batal!")
     }
 }
---
+++ src/oop_00000133876_AdamRifqyHajat/week02/Loan.kt
@@ -0,0 +1,16 @@
+package oop_00000133876_AdamRifqyHajat.week02.oop_00000133876_AdamRifqyHajat.week02
+
+class Loan (
+    val bookTitle: String,
+    val borrower: String,
+    var loanDuration: Int = 1 // Dalam hari
+) {
+    fun calculateFine() : Int {
+        if (loanDuration > 3){
+            return (loanDuration - 3) * 2000
+        }
+        else{
+            return 0
+        }
+    }
+}
