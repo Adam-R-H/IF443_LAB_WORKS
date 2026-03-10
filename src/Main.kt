31e1d0dcb9f8c5063efad00d84ee3794f95499a6
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-03-10 11:28:14 +0700

    week 03: (task2) implement Player testing in main

--- src/Main.kt
+++ src/Main.kt
@@ -5,4 +5,8 @@ fun main() {
     Senjata.damage = -50
     Senjata.damage = 9999
     println("Tier: ${Senjata.tier}")
+
+    val Pemain = Player(username = "Pedang")
+    Pemain.addXp(50)
+    Pemain.addXp(60)
 }
