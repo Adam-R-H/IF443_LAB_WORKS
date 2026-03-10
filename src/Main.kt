be8567e798c72f57911c7fbafc114ad732b89345
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-03-10 11:42:58 +0700

    week 04: test Vehicle and Car in main

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,14 @@
-package oop_00000133876_AdamRifqyHajat.week03
+package oop_00000133876_AdamRifqyHajat.week04
 
 fun main() {
-   val Senjata = Weapon("Budi")
-    Senjata.damage = -50
-    Senjata.damage = 9999
-    println("Tier: ${Senjata.tier}")
+   println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
 
-    val Pemain = Player(username = "Pedang")
-    Pemain.addXp(50)
-    Pemain.addXp(60)
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk() // Memanggil method milik sendiri
+    myCar.honk() // Memanggil method yang sudah di-override
+    myCar.accelerate() // Memanggil gabungan method Parent dan Child
 }
