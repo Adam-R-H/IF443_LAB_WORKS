43f042325699bea6a1bd0ff967d3dd207fd067f5
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-03-10 13:49:25 +0700

    week 06: create compound interfaces

---
+++ src/oop_00000133876_AdamRifqyHajat/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000133876_AdamRifqyHajat.week06
+
+// Interface = Can-do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable{
+    fun chargeBattery()
+}
