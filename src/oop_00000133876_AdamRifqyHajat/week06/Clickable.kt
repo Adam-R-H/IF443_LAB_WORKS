52cdce3cd47df3f1fde5aeca89863ee975fb7013
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-03-10 13:37:58 +0700

    week 06: (trial) create Clickable interface with backing field error

--- src/oop_00000133876_AdamRifqyHajat/week06/Clickable.kt
+++ src/oop_00000133876_AdamRifqyHajat/week06/Clickable.kt
@@ -1,9 +1,6 @@
 package oop_00000133876_AdamRifqyHajat.week05.oop_00000133876_AdamRifqyHajat.week06
 
 interface Clickable {
-    // ERROR: Property in an interface canot have a backing field
-    val name: String = "Tombol Rahasia"
-
-    // Function without body (Implicitly Abstract)
+    val name: String
     fun click()
 }
