609e9198cd3b96ee45826388ec8d88820054e606
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-03-10 12:54:46 +0700

    week 05: (task2) implement payment polymorphism

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,14 @@
 package oop_00000133876_AdamRifqyHajat.week05
 
+import kotlin.system.exitProcess
+
 fun main() {
-   val Helper1 = MathHelper()
-    println("Luas Square 10 = ${Helper1.hitungLuas(10)}\n" +
-            "Luas Rectangle 10x20 = ${Helper1.hitungLuas(10, 20)}\n" +
-            "Luas Circle 10 = ${Helper1.hitungLuas(10.toDouble())}")
+   val Dompet = EWallet("Budi", 50000.0)
+    val Kartu = CreditCard("Siti", 100000.0)
+
+    val daftar : List<PaymentMethod> = listOf(Dompet, Kartu)
+
+    for (payment in daftar){
+        payment.processPayment(75000.0)
+    }
 }
