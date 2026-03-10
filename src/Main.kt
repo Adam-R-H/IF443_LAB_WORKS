67b1dfe2113319983215e901d605872e90f21690
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-03-10 12:38:03 +0700

    week 05: (task1) test MathHelper overloading in main

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,8 @@
 package oop_00000133876_AdamRifqyHajat.week05
 
 fun main() {
-   val dosen1 = Dosen("Pak Alex", "0123456")
-    val admin1 = Admin("Bu Siti")
-
-    // Polymorphic Collection: List yang berisi tipe Parent, tapi isinya Objek Anak
-    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
-
-    println("=== AKTIVITAS PEGAWAI ===")
-    for (pegawai in daftarPegawai) {
-        // Pemanggilan Runtime Polymorphism
-        pegawai.bekerja()
-
-        // pegawai.mengajar() // INI AKAN ERROR karena tipe referensinya adalah Pegawai
-        // Smart casting dengan is dan when
-        when (pegawai) {
-            is Dosen -> {
-                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
-                pegawai.mengajar() // Smart cast! Tidak perlu manual casting (as)
-            }
-            is Admin -> {
-                println("=> Terdeteksi sebagai Admin")
-                pegawai.doAdminWork()
-            }
-        }
-        println("-------------------------")
-    }
+   val Helper1 = MathHelper()
+    println("Luas Square 10 = ${Helper1.hitungLuas(10)}\n" +
+            "Luas Rectangle 10x20 = ${Helper1.hitungLuas(10, 20)}\n" +
+            "Luas Circle 10 = ${Helper1.hitungLuas(10.toDouble())}")
 }
