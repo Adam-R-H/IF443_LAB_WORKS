10a07b58a57a402a15fb0445eb3d9935f2be1247
Adam-R-H <adam.rifqy@student.umn.ac.id>
2026-04-21 12:33:04 +0800

    week 07: (task) simulate rarity and factory instantiation

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,9 @@ package oop_00000133876_AdamRifqyHajat.week07
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println("Rarity legendary = ${ItemRarity.LEGENDARY.dropChance}%")
+    val StarterSword = Weapon.forgeStarterSword()
+
+    println("Membuat senjata dengan detail: ${StarterSword.item}")
 }
